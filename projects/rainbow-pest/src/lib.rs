02e2274808740d99216af950d2bc12a795f74ee6
Aster <galaster@foxmail.com>
2022-07-01 18:04:52 +0800

    Remove redundant dictionary types

--- projects/rainbow-pest/src/lib.rs
+++ projects/rainbow-pest/src/lib.rs
@@ -1,10 +1,12 @@
-mod arc;
-
-pub use arc::{ArcParser, Rule};
 pub use pest::{
     self,
     error::Error,
     iterators::{Pair, Pairs},
-    prec_climber::{Assoc, Operator, PrecClimber},
-    Parser, Span,
+    Parser,
+    prec_climber::{Assoc, Operator, PrecClimber}, Span,
 };
+
+pub use rainbow::{RainbowParser, Rule};
+
+mod rainbow;
+
--- projects/rainbow-pest/tests/main.rs
+++ projects/rainbow-pest/tests/main.rs
@@ -13,18 +13,18 @@ fn ready() {
 #[test]
 #[ignore]
 pub fn gen_parser() {
-    let pest = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "./arc.pest"));
-    let rs = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "./src/arc.rs"));
+    let pest = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "./rainbow.pest"));
+    let rs = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "./src/rainbow.rs"));
 
     let derived = {
         let path = pest.to_string_lossy();
         let pest = quote! {
             #[grammar = #path]
-            pub struct ArcParser;
+            pub struct RainbowParser;
         };
         derive_parser(pest, false)
     };
     let mut file = File::create(rs).unwrap();
-    let out = format!("pub struct ArcParser;{}", derived);
+    let out = format!("pub struct RainbowParser;{}", derived);
     writeln!(file, "{}", out).unwrap();
 }
